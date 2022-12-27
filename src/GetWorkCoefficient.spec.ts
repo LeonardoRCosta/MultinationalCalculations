025ece7cbc86fb47944f9e0283283125c0235646
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-27 12:22:48 -0300

    fix: uses the static method

--- src/GetWorkCoefficient.spec.ts
+++ src/GetWorkCoefficient.spec.ts
@@ -1,19 +1,13 @@
 import { GetWorkCoefficient } from './GetWorkCoefficient';
 
 describe('Get the work coefficient', () => {
-  let getWorkCoefficient: GetWorkCoefficient;
-
-  beforeEach(() => {
-    getWorkCoefficient = new GetWorkCoefficient();
-  });
-
   it('should be able to return the right work coefficient', () => {
-    expect(getWorkCoefficient.execute('matutino')).toEqual(0.13);
-    expect(getWorkCoefficient.execute('vespertino')).toEqual(0.04);
-    expect(getWorkCoefficient.execute('noturno')).toEqual(0.08);
+    expect(GetWorkCoefficient.execute('matutino')).toEqual(0.13);
+    expect(GetWorkCoefficient.execute('vespertino')).toEqual(0.04);
+    expect(GetWorkCoefficient.execute('noturno')).toEqual(0.08);
   });
 
-  it('should return undefined if the work shift does not exist', () => {
-    expect(getWorkCoefficient.execute('invalidWorkshift')).toBeUndefined();
+  it('should be able to throw an error if the work shift does not exist', () => {
+    expect(() => GetWorkCoefficient.execute('invalidWorkshift')).toThrowError();
   });
 });
