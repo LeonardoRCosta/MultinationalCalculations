89841ae99e930c907f9d4d0c9787ffbb6bc72e96
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-27 12:19:14 -0300

    test: tests calculate min salary use case

---
+++ src/CalculateMinSalary.spec.ts
@@ -0,0 +1,15 @@
+import { CalculateMinSalary } from './CalculateMinSalary';
+
+describe('Calculate the minimum salary', () => {
+  it('should be able to calculate the minimum salary based on the employee position', () => {
+    expect(CalculateMinSalary.execute('operario')).toBeLessThanOrEqual(2500);
+    expect(CalculateMinSalary.execute('operario')).toBeGreaterThanOrEqual(750);
+
+    expect(CalculateMinSalary.execute('gerente')).toBeLessThanOrEqual(4600);
+    expect(CalculateMinSalary.execute('gerente')).toBeGreaterThanOrEqual(1400);
+  });
+
+  it('should be able to throw an error if the passed employee position does not exist', () => {
+    expect(() => CalculateMinSalary.execute('invalidPosition')).toThrowError();
+  });
+});
