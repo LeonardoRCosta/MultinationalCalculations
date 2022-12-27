c9553e38f0f42d4138ca64c60c7c46e4b2af2300
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-27 12:22:01 -0300

    fix: uses the static methods

--- src/CalculateTaxRate.spec.ts
+++ src/CalculateTaxRate.spec.ts
@@ -1,48 +1,42 @@
 import { CalculateTaxRate } from './CalculateTaxRate';
 
 describe('Calculate the tax rate', () => {
-  let calculateTaxRate: CalculateTaxRate;
-
-  beforeEach(() => {
-    calculateTaxRate = new CalculateTaxRate();
-  });
-
   it('should be able to calculate the tax rate based on the gross salary', () => {
     expect(
-      calculateTaxRate.execute({
+      CalculateTaxRate.execute({
         employeePosition: 'operario',
         grossSalary: 900,
       })
     ).toEqual(0.07);
 
     expect(
-      calculateTaxRate.execute({
+      CalculateTaxRate.execute({
         employeePosition: 'operario',
         grossSalary: 1000,
       })
     ).toEqual(0.13);
 
     expect(
-      calculateTaxRate.execute({
+      CalculateTaxRate.execute({
         employeePosition: 'gerente',
         grossSalary: 1600,
       })
     ).toEqual(0.15);
 
     expect(
-      calculateTaxRate.execute({
+      CalculateTaxRate.execute({
         employeePosition: 'gerente',
         grossSalary: 1450,
       })
     ).toEqual(0.1);
   });
 
-  it('should return undefined if the employee position does not exist', () => {
-    expect(
-      calculateTaxRate.execute({
+  it('should be able to throw an error if the employee position does not exist', () => {
+    expect(() =>
+      CalculateTaxRate.execute({
         employeePosition: 'invalid employee position',
         grossSalary: 1450,
       })
-    ).toBeUndefined();
+    ).toThrowError();
   });
 });
